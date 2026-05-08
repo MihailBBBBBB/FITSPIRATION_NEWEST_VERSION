0bd2b6cd5a9802f8e2879f6f7dc9af05dda5e792
MihailBBBBBB <mb00098@rvt.lv>
2026-05-08 17:53:27 +0300

    img fix

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -3,6 +3,10 @@ set -eu
 
 export FITSPIRATION_INTERNAL_WS_PORT="${FITSPIRATION_INTERNAL_WS_PORT:-8081}"
 
+mkdir -p /var/www/html/images
+chown -R www-data:www-data /var/www/html/images || true
+chmod -R 0777 /var/www/html/images || true
+
 php /var/www/html/websocket/messages-server.php >> /proc/self/fd/1 2>> /proc/self/fd/2 &
 
 exec apache2-foreground
